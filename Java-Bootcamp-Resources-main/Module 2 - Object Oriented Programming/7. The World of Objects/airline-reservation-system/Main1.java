761346b20edc65ed22dd69c49e99650ac4e07bb4
munir sanzar <msanzar87@gmail.com>
2021-10-07 03:40:48 -0400

    Person1 Completed

---
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main1.java
@@ -0,0 +1,15 @@
+public class Main1 {
+    public static void main(String[] args) {
+        Person1 jack = new Person1();
+//        jack.name;
+//        jack.dateOfBirth;
+//        jack.nationality;
+//        jack.seatNumber;
+//        jack.passport;
+        System.out.println(jack.name);
+        System.out.println(jack.dateOfBirth);
+        System.out.println(jack.nationality);
+        System.out.println(jack.seatNumber);
+        System.out.println(jack.passport);
+    }
+}
---
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person1.java
@@ -0,0 +1,7 @@
+public class Person1 {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    String[] passport;
+    int seatNumber;
+}
