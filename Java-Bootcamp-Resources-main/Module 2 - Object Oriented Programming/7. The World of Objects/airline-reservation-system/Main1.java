67f368bd2f07c36b0c4e36d8b2ca896fb7903487
munir sanzar <msanzar87@gmail.com>
2021-10-08 01:53:15 -0400

    Person4 Completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main1.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main1.java
@@ -1,10 +1,13 @@
 public class Main1 {
     public static void main(String[] args) {
         Person1 jack = new Person1("Munir","USA","01/11/1111",4);
-
+        Person1 person2 = new Person1("Buzz","USA","2/22/2222", 22);
+        person2.setSeatNumber(1);
+        person2.setName("John");
         jack.setSeatNumber(10);
         System.out.println("Name: "+jack.getName()+ "\n" + "Nationality: "+jack.getNationality()+
                 "\n" + "Date of Birth: "+jack.getDateOfBirth()+ "\n" + "Seat number: "+jack.getSeatNumber());
-
+        System.out.println("Name: "+person2.getName()+ "\n" + "Nationality: "+person2.getNationality()+
+                "\n" + "Date of Birth: "+person2.getDateOfBirth()+ "\n" + "Seat number: "+person2.getSeatNumber());
     }
 }
