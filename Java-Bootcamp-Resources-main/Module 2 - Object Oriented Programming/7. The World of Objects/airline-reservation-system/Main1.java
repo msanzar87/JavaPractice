fdda4c7ace5f868342dcfdc82e2d9e93f17a471d
munir sanzar <msanzar87@gmail.com>
2021-10-08 01:36:41 -0400

    Person3 Completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main1.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main1.java
@@ -1,15 +1,10 @@
 public class Main1 {
     public static void main(String[] args) {
-        Person1 jack = new Person1();
-//        jack.name;
-//        jack.dateOfBirth;
-//        jack.nationality;
-//        jack.seatNumber;
-//        jack.passport;
-        System.out.println(jack.name);
-        System.out.println(jack.dateOfBirth);
-        System.out.println(jack.nationality);
-        System.out.println(jack.seatNumber);
-        System.out.println(jack.passport);
+        Person1 jack = new Person1("Munir","USA","01/11/1111",4);
+
+        jack.setSeatNumber(10);
+        System.out.println("Name: "+jack.getName()+ "\n" + "Nationality: "+jack.getNationality()+
+                "\n" + "Date of Birth: "+jack.getDateOfBirth()+ "\n" + "Seat number: "+jack.getSeatNumber());
+
     }
 }
--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person1.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person1.java
@@ -1,7 +1,50 @@
 public class Person1 {
-    String name;
-    String nationality;
-    String dateOfBirth;
-    String[] passport;
-    int seatNumber;
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
+    private int seatNumber;
+
+
+    public Person1(){}
+    public Person1(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
 }
+
+
+
