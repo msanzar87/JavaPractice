d9232887b66a321bddd719fe0435418170bf2ee8
munir sanzar <msanzar87@gmail.com>
2021-11-04 16:57:31 -0400

    Overrode shirts  Completed

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Pants.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Pants.java
@@ -1,1 +1,15 @@
 package models;
+
+public class Pants{
+    private String waist;
+    private String color;
+    private double price;
+    private String brand;
+
+    public Pants(String waist, double price, String color, String brand){
+        this.waist = waist;
+        this.price = price;
+        this.color = color;
+        this.brand = brand;
+    }
+}
--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Shirt.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/10. Inheritance/products/models/Shirt.java
@@ -1,1 +1,15 @@
 package models;
+
+public class Shirt{
+    private String size;
+    private String color;
+    private double price;
+    private String brand;
+
+    public Shirt(String size, double price, String color, String brand){
+        this.size = size;
+        this.price = price;
+        this.color = color;
+        this.brand = brand;
+    }
+}
